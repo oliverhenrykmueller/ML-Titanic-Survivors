e2afdce39699038b22b1bc168e4580231ee7aabe
oliverhenrykmueller <oliver.mueller.de@gmail.com>
2021-04-07 23:16:14 +0200

    Improved and created more features. Added new models. Cleaned up.

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
-import numpy as np
-import sklearn
 import csv
+import numpy as np
 import pandas as pd
 from sklearn import svm
 import logging as lg
@@ -10,30 +9,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelBinarizer
 from sklearn.model_selection import GridSearchCV
+import re
 import matplotlib.pyplot as plt
 import seaborn as sns;
 
 sns.set(style="ticks", color_codes=True)
-
-
-def preprocess(data):
-    return ""
-
-
-def checkEmptyValues(df, column):
-    array = []
-    i = 0
-    # print pd.isna(df["PassengerId"])
-
-    for x in pd.isna(df[column]):
-        if x == True:
-            array.append(x)
-    if not array:
-        empty = True
-    else:
-        empty = False
-
-    return empty, array
 
 
 def data_explore(data):
@@ -133,7 +113,7 @@ if __name__ == '__main__':
         "/media/Jung/Desktop/Arbeit & Karierre/Data Analytics & CS Projekte/Kaggle Competitions/Titanic/Data/train.csv")
     test = pd.read_csv(
         "/media/Jung/Desktop/Arbeit & Karierre/Data Analytics & CS Projekte/Kaggle Competitions/Titanic/Data/test.csv")
-    data_complete = [train, test]
+
 
     # Show information of the data #################################################
     print("TRAINING DATA")
@@ -149,13 +129,12 @@ if __name__ == '__main__':
     # Preprocessing #################################################
     # Merge both test and train data for additional imputation data
     data_merged = pd.concat([train.drop(["Survived"], axis=1), test], axis=0)
+    data_complete = [train, test, data_merged]
 
     # Save passengerID-list from testdata for later ###
     test_np_id = test["PassengerId"].to_numpy()
 
-    # Drop unnecessary columns ###
-    train.drop(["PassengerId", "Ticket"], axis=1, inplace=True)
-    test.drop(["PassengerId", "Ticket"], axis=1, inplace=True)
+
 
     # Imputation of missing values ###
 
@@ -211,7 +190,7 @@ if __name__ == '__main__':
 
     #raise Exception
 
-    # Feature Engineering, create more significant features #################################################
+    # Feature Engineering #################################################
     # Transformation of Categorical Data ##
 
     # New Feature: Familysize :
@@ -224,6 +203,7 @@ if __name__ == '__main__':
 
     # New Feature: Age ranges
     for dataset in data_complete:
+        ## OLD
         # dataset.loc[dataset["Age"] <= 2 , "Age_infant"] = True
         # dataset.loc[(dataset["Age"] > 2) & (dataset["Age"] <= 12), "Age_child"] = True
         # dataset.loc[(dataset["Age"] > 12 )& (dataset["Age"] <= 18), "Age_teen"] = True
@@ -258,46 +238,106 @@ if __name__ == '__main__':
             {0: 'Infant', 1: 'Small_Child', 2: "Child", 3: "Teen", 4: "Teen_Adult", 5: "Young_Adult", 6: "Adult",
              7: "Middleage_Adult", 8: "Old_adult", 9: "Senior"}, inplace=True)
 
-    # New Feature: Deck Level, depending on the Ticket letter
+    # New Feature: Deck Level, depending on the First letter, missing data will be filled with "U" with unknown
+
+    a = train.sort_values("Cabin")["Cabin"]
+
+    for dataset in data_complete:
+        dataset['Decklevel'] = dataset['Cabin'].str.replace(r'\d', '')
+        dataset.Decklevel.fillna("U", inplace=True)
+        for row in dataset.Decklevel.items():
+            level = row[1][0]
+            index = row[0]
+            dataset.at[index, "Decklevel"] = level
+
+    # Drop the Cabin column since its not useful anymore
+    for dataset in data_complete:
+        dataset.drop("Cabin", axis=1)
+
+    # New feature: Fare categorization #
+    # Fare per Person since Fare counts for whole Families#
+    # Look at data how Fare is handled in relatives
+    print(train.loc[train["Familysize"] > 4])
+    print(train.loc[train.Name.str.startswith("Asplund")])
+
+    for dataset in data_complete:
+        dataset["FarePerson"] = dataset["Fare"] / (dataset["Familysize"]+1)
+
+    data_merged.hist("Fare", bins=50)
+    #plt.show()
+
+    # Binning 2 ranges of data, the low ones, and the medium to high ones
+    lowfares = pd.qcut(data_merged.loc[data_merged["Fare"]< 90 ]["Fare"], 4)
+    medium_highfares = pd.qcut(data_merged.loc[(data_merged["Fare"] > 90) & (data_merged["Fare"] < 250)]["Fare"], 2)
+    print(lowfares.value_counts())
+    print(medium_highfares.value_counts())
+
+    # Data is mostly  distributed at 0-90, then lightly populated from ~90-250, finally has an outlier at 500
+
+    for dataset in data_complete:
+        dataset.loc[dataset["Fare"] < 7.896, "Fare"] = 0
+        dataset.loc[(dataset["Fare"] >= 7.896) & (dataset["Fare"] < 13.0), "Fare"] = 1
+        dataset.loc[(dataset["Fare"] >= 13.0) & (dataset["Fare"] < 26.55), "Fare"] = 2
+        dataset.loc[(dataset["Fare"] >= 26.55) & (dataset["Fare"] < 89.104), "Fare"] = 3
+        dataset.loc[(dataset["Fare"] >= 89.104) & (dataset["Fare"] < 146.521), "Fare"] = 4
+        dataset.loc[(dataset["Fare"] >= 146.521) & (dataset["Fare"] < 247.521), "Fare"] = 5
+        dataset.loc[(dataset["Fare"] >= 247.521), "Fare"] = 6
+        dataset['Fare'].replace(
+            {0: 'Extremely_low', 1: 'Very_Low', 2: "Low", 3: "Medium", 4: "High", 5: "Very High", 6: "Extremely_high"}, inplace=True)
+
+    print(train.head())
+
+    # New Feature: Title
+    # Like Miss, Mr., Mrs and rarer titles like Sir, military ranks, etc.
+
+    titles = train.Name.str.extract(r"([A-Za-z]+)\.")
+    titles_higher = ["Dr", "Rev", "Major", "Col", "Mlle", "Mme", "Ms", "Capt", "Lady", "Jonkheer", "Don", "Countess", "Sir"]
+
+    for dataset in data_complete:
+        dataset["Title"] = dataset.Name.str.extract(r"([A-Za-z]+)\.", expand=False)
+        dataset["Title"] = dataset.Title.replace(titles_higher, "High_title")
+
+    print(train.Title.unique())
 
 
-    # PClass transformation to prepare for One hot encoding
+    # Feature Vector Processing #################################################
+
+    ## Drop all uneccessary columns
+    print("Feature Vector Processing")
+    print(train)
+
+
+    for dataset in data_complete:
+        dataset.drop(["Cabin","Name","PassengerId", "Ticket"], axis=1,inplace=True)
+
+    # Transformation of categorical data to prepare for One hot encoding ##
+    # Pclass
     for dataset in data_complete:
         dataset.loc[dataset["Pclass"] == 1, "Pclass"] = "1"
         dataset.loc[dataset["Pclass"] == 2, "Pclass"] = "2"
         dataset.loc[dataset["Pclass"] == 3, "Pclass"] = "3"
 
-    # New feature: Fare categorization
-
-    #for dataset in data_complete:
-    #    dataset.loc[(dataset["Fare"] < 7)] = 0
-    #    dataset.loc[(dataset["Fare"] >= 7) & (dataset["Fare"] < 20)]
-
     print(train)
 
-    # Feature Vector Processing #################################################
+    # Feature selection #########
 
-    # Categorical data: Transform categorical columns from numerical data to categories
 
     # Scaling numerical data ##
 
-    train_numerical_features = list(train.drop(["Survived"],axis=1).select_dtypes(include=['int64', 'float64', 'int32']).columns)
-    del train_numerical_features[0]
+    numerical_features = list(train.drop(["Survived"], axis=1).select_dtypes(include=['int64', 'float64', 'int32']).columns)
+    print(numerical_features)
 
     ss_scaler = StandardScaler()
+    #ss_scaler = MinMaxScaler()
     train_ss = pd.DataFrame(data=train)
     test_ss = pd.DataFrame(data=test)
-    traintestset = pd.concat([train, test], axis=1)
-    print("TRAINTESTSET")
-    print(traintestset)
-    ss_scaler.fit(traintestset)
-    print()
 
-    train_ss[train_numerical_features] = ss_scaler.transform(train_ss[train_numerical_features])
-    test_ss[train_numerical_features] = ss_scaler.transform(test_ss[train_numerical_features])
+    ss_scaler.fit(train[numerical_features])
 
-    print("SCALED")
-    print(test_ss)
+    train_ss[numerical_features] = ss_scaler.transform(train_ss[numerical_features])
+    test_ss[numerical_features] = ss_scaler.transform(test_ss[numerical_features])
+
+    print("DATA SCALED")
 
     print(train.select_dtypes(include=['object']).columns)
 
@@ -310,7 +350,7 @@ if __name__ == '__main__':
         train_ss.drop(col, axis=1, inplace=True)
         test_ss.drop(col, axis=1, inplace=True)
 
-    # Turn String features into Numeric Values (old)
+    # OLD: Turn String features into Numeric Values
     # train['Embarked'] = pd.factorize(train.Embarked)[0]
     # train["Sex"] = pd.factorize(train.Sex)[0]
 
@@ -331,26 +371,41 @@ if __name__ == '__main__':
 
     # Setting up hyperparameters for Gridsearch
 
-    parameters = {"penalty": ["l2"],
+    parameters_reg = {"penalty": ["l2"],
                   "C": [1.0, 2.0, 10.0, 100.0],
-                  "max_iter": [100, 200, 500, 1000]
+                  "max_iter": [300,500, 1000, 2000]
                   }
 
+    parameters_svm = {"loss" : ["hinge","squared_hinge"],
+                      "C":[1.0,1.5,2.0,5,10],
+                      "max_iter":[1000,2000,5000,10000,20000]
+                      }
+
+    parameters_svm = {"kernel": ["linear", "rbf","sigmoid"],
+                      "C": [1.0, 1.5, 2.0, 5, 10],
+                      "gamma":["scale","auto"]
+                      }
+
     # Fitting the train data
-    # clf = RandomForestClassifier(n_estimators=100)
-    # clf.fit(X, y)
+    clf = RandomForestClassifier(n_estimators=100)
+
+    clf_svc = svm.SVC()
+    clf_linear_svm = svm.LinearSVC()
+
+    grid_svm = GridSearchCV(clf_svc,param_grid=parameters_svm)
+    #grid_svm = svm.LinearSVC()
 
     logreg = LogisticRegression()
     scoring = ["r2", "explained_variance"]
-    grid = GridSearchCV(logreg, parameters, scoring=scoring, refit="r2", cv=5)
+    #grid = GridSearchCV(logreg, parameters, scoring=scoring, refit="r2", cv=5)
 
-    grid.fit(X, y)
-    print(grid)
-    # clf.fit(X, y)
+
+
+    grid_svm.fit(X, y)
 
     # Prediction of test set ##
-    # predictions = clf.predict(X_test)
-    predictions = grid.predict(X_test)
+    #predictions = clf.predict(X_test)
+    predictions = grid_svm.predict(X_test)
 
     # Check if initial testset(id) and predictions have the same length
     if len(test_np_id) != len(predictions):
@@ -366,8 +421,10 @@ if __name__ == '__main__':
     with open("predictions.csv", "w") as predictfile:
         cwriter = csv.writer(predictfile)
         cwriter.writerow(("PassengerId", "Survived"))
+        np.savetxt(predictfile, csv_predictions, delimiter=',', fmt='%d')
+
     #csv_file = open("predictions.csv", 'ab')
-    np.savetxt(predictfile, csv_predictions, delimiter=',', fmt='%d')
     #csv_file.close()
 
-    print(train_ss.head(10))
+    print(grid_svm.best_estimator_)
+    print(grid_svm.best_params_)
